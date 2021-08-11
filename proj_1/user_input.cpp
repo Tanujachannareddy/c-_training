fe2db445f5e3c4b91b9645c02edc8a0270af15b6
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-11 15:05:01 +0530

    Create user_input.cpp

---
+++ proj_1/user_input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+
+int main() {
+	int slices;
+	cout << "How many pizzas you want to eat: ";
+	cin >> slices;
+	cout << "I want "<< slices << " slices"<< std::endl;
+ 
+}
