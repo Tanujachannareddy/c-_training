af7f9889e7226d31260178ed1ef24576e5ae1c54
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-12 22:05:02 +0530

    Create integral_data_types.cpp

---
+++ proj_1/integral_data_types.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<climits>
+using std::cout;
+using std::cin;
+
+int main() {
+
+	short a;
+	int b;
+	long c;
+	long long d;
+	unsigned short aa;
+	unsigned int bb;
+	unsigned long cc;
+	unsigned long long dd;
+	cout << SHRT_MAX << SHRT_MIN << "\n";
+	cout << USHRT_MAX << "\n";
+	cout << INT_MAX << INT_MIN << "\n";
+	cout << UINT_MAX << "\n";
+	cout << LONG_MAX << LONG_MIN << "\n";
+	cout << ULONG_MAX << "\n";
+	cout << LLONG_MAX << LLONG_MIN << "\n";
+	cout << ULLONG_MAX << "\n";
+}
