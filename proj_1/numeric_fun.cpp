bc84b4c3b2aeee196f258a7f69489f7c19a4dee6
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-13 16:06:27 +0530

    Create numeric_fun.cpp

---
+++ proj_1/numeric_fun.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+	cout << sqrt(25) << endl;
+	cout << sqrt(-25) << endl;
+	cout << pow(5, 99999) << endl;
+	cout << remainder(5, 4) << endl;
+	cout << remainder(5, 4.5) << endl;
+	cout << fmod(5, 4) << endl;
+	cout << fmax(5, 4.5) << endl;
+	cout << fmin(5, 4.5) << endl;
+	cout << trunc(4.5) << endl;
+	cout << floor(4.5) << endl;
+	cout << floor(-4.5) << endl;
+	cout << trunc(-4.5) << endl;
+	cout << ceil(-4.5) << endl;
+	cout << ceil(4.5) << endl;
+	cout << round(-4.5) << endl;
+	cout << round(4.5) << endl;
+	
+}
