4f1316b20e63cc57b43a61857509d9e4aa92ee12
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-13 16:21:16 +0530

    Create string_cstring.cpp

---
+++ proj_1/string_cstring.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	string  name = "tanuja";
+	name = name + "kalli";
+	cout << name << endl;
+	cout << name[6] << endl;
+
+	char x[] = "tanu";//c string
+	
+	cout << x[0] << endl;
+}
