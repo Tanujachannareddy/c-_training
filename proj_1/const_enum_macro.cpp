c8f17860b7fd0d83a8c38ef21ac311d0172f4e76
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-13 15:27:24 +0530

    Create const_enum_macro.cpp

---
+++ proj_1/const_enum_macro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#define A 60
+
+int main() {
+
+	const int x = 5;
+	enum {y=50};
+	cout << x << endl;
+	cout << y << endl;
+	cout << A << endl;
+
+}
