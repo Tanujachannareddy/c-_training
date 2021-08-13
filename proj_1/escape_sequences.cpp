4bf9d6f39eb2a1553698cb11785d18660eff4dc1
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-13 14:36:01 +0530

    Create escape_sequences.cpp

---
+++ proj_1/escape_sequences.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+
+int main() {
+
+	//escape sequences
+	cout << "tanuja\tkalli"<< "\n";
+	cout << "c:\\windows\\system32" << "\n";
+	cout << "hello\bthere" << "\n";//backspace
+	cout << "hello\a" << "\n";
+	cout << "hello\"there\"" << "\n";
+	cout << "isn\'t it" << "\n";
+	cout << "hello\0" << "\n";
+}
