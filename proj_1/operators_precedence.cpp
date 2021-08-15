e35521ae5dcf4a27cbde7857eda491702644a971
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-15 17:11:57 +0530

    Create operators_precedence.cpp

---
+++ proj_1/operators_precedence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+	int x = 5 + 5;
+	int y = 10 * 10;
+	int z = 10 / 2;
+	int a = 10 / 3;
+	int b = 10 / 3.0;
+	int c = 10 % 4;
+	std::cout << x <<"\n" << y <<"\n" << z <<"\n" << a <<"\n" << b <<"\n" << c;
+	int t, j;
+	j = t = 100;
+	std::cout << t <<"\n" << j;
+	(j = t) = 200;
+	std::cout << t <<"\n" << j;
+}
+
+
+	
