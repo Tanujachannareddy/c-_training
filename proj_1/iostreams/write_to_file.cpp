405a434b0c80ae7f9eabebd53a5176f665bc01ac
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-17 14:49:33 +0530

    Update write_to_file.cpp

--- proj_1/iostreams/write_to_file.cpp
+++ proj_1/iostreams/write_to_file.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+int main() 
+{
+    std::string file_name = "practice.txt";
+    std::ofstream  file(file_name,std::ios::app);
+    std::vector<std::string> names;
+    names.push_back("tanuja");
+    names.push_back("mahith");
+    names.push_back("venkat");
+    if (file.is_open())
+    {
+        std::cout << "file opened";
+    }
 
+    /*for (std::string name : names) 
+    {
+        file << name << std::endl;
+    }*/
+}
