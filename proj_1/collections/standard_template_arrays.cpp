58ea6ababfd769998581b17afe45cad83ed2d322
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-17 12:14:24 +0530

    Create standard_template_arrays.cpp

---
+++ proj_1/collections/standard_template_arrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<array>
+void print_array(std::array<int, 3> data)
+{
+	for (int i = 0; i < data.size(); i++)
+	{
+		std::cout << data[i] << "\t";
+	}
+	std::cout << "\n";
+}
+int main()
+{
+	std::array<int, 3> data = { 1,2,3 };
+	print_array(data);
+}
+
+
+
