a9508c31c16eef1e6fc361ccb1396bdaed4f1e19
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-17 15:25:42 +0530

    Create read_file_while.cpp

---
+++ proj_1/iostreams/read_file_while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+int main() 
+{
+	std::ifstream file("practice.txt");
+	std::vector < std::string > names;
+	std::string input;
+	while (file >> input) 
+	{
+		names.push_back(input);
+	}
+	for (std::string name : names) 
+	{
+		std::cout << name << std::endl;
+	}
+}
