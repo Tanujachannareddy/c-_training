68c080f6370b1b060de743befcbff43b0f5d1fa9
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-17 12:00:22 +0530

    Update vectors_call_by_value_and_reference.cpp

--- proj_1/collections/vectors_call_by_value_and_reference.cpp
+++ proj_1/collections/vectors_call_by_value_and_reference.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+#include<vector>
+#include<ctime>
+#include<cstdlib>
+void print_array(std::vector<int> &data)
+{
+	data.push_back(6);
+	for (int i = 0; i < data.size(); i++)
+	{
+		std::cout << data[i] << "\t";
+	}
+	std::cout << "\n";
+}
+int main()
+{
+	std::vector<int> array = { 1,2,3 };
+	print_array(array);
+	print_array(array);
+	print_array(array);
+	print_array(array);
+}
+
+
+
