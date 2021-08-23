700b8cbeafadc104b2d854a12098c2f6a15e5a1f
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-23 16:30:40 +0530

    Update multi_dimensional_array.cpp

--- proj_1/multi_dimensional_array.cpp
+++ proj_1/multi_dimensional_array.cpp
@@ -1,4 +1,14 @@
 #include<iostream>
+//#include<vector>
+//int main() {
+//	std::vector<std::vector<int>> a = { {0,1,3},{1,2,3},{5,6,6} };
+//	for (int r = 0; r < 3; r++) {
+//		for (int c = 0; c < 3; c++) {
+//			std::cout << a[r][c] << "\t";
+//		}
+//		std::cout << "\n";
+//	}
+//}
 int main() {
 	int a[3][3] = { {0,1,3},{1,2,3},{5,6,6} };
 	for (int r = 0; r < 3; r++) {
