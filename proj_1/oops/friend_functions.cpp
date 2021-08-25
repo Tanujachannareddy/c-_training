4996cdd2d52fba1fbd88c75c6d0581afcd934e20
Tanujachannareddy <88763418+Tanujachannareddy@users.noreply.github.com>
2021-08-25 18:00:34 +0530

    Create friend_functions.cpp

---
+++ proj_1/oops/friend_functions.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+class Position {
+	int z = 30;
+public:
+	int x = 10;
+	int y =20;
+	Position operator + (Position pos) {
+		Position new_pos;
+		new_pos.x = x + pos.x;
+		new_pos.y = y + pos.y;
+		return new_pos;
+
+	}
+	bool operator ==(Position pos) {
+		if (x==pos.x || y==pos.y) {
+			return true;
+		}
+		return false;
+	}
+	friend std::ostream& operator << (std::ostream& output, Position pos);
+	friend std::istream& operator >> (std::istream& input, Position& pos);
+};
+std::ostream& operator << (std::ostream& output, Position pos)
+{
+	output << "x :" << pos.x << "\n" << "y:" << pos.y << "\nz: "<< pos.z;
+	return output;
+}
+std::istream& operator >> (std::istream& input, Position& pos)
+{
+	input >> pos.x >> pos.y>>pos.z;
+	return input;
+}
+int main() {
+	Position pos1, pos2;
+	Position pos3 = pos1 + pos2;
+	std::cout << pos3.x << "\t" << pos3.y;
+	pos1.x = 30;
+	if (pos1 ==pos2){
+		std::cout << "they are same";
+	}
+	std::cin >> pos3;
+	std::cout << pos3;
+	
+}
